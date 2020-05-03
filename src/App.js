6cda68c099aa85c1c1943e7d75041908e6ed83cf
Keivin <keivinbr13@gmail.com>
2020-05-02 19:09:59 -0500

    Contador Hecho

--- src/App.js
+++ src/App.js
@@ -2,11 +2,21 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  state={
+    contador: 0,
+  }
+
+  contar = (e) =>{
+    this.setState({
+      contador: e.target.value.length,
+    });
+  }
+
   render() {
     return (
       <div className="container">
-        <textarea rows="3"></textarea>
-        <div className="counter">0</div>
+        <textarea rows="3" onChange={this.contar} ></textarea>
+        <div className="counter">{this.state.contador}</div>
       </div>
     );
   }
